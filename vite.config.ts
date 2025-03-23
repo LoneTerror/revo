203845c64fc325346f92634529b8f1dee2cf71fb
Anilabh Barua <anilabh.barua@gmail.com>
2025-03-24 01:21:30 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["hackdays.tech"],
+    allowedHosts: ["test.hackdays.online"],
   },
 })
